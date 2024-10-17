fix: further fixes for normalization

src/careamics/dataset/dataset_utils/running_stats.py
@@ -33,14 +33,14 @@ def compute_normalization_stats(
         # Define the list of axes excluding the channel axis
         axes = tuple(np.delete(np.arange(image.ndim), 1))
         stats = (
-            np.mean(image, axis=axes), 
-            np.std(image, axis=axes)
+            np.mean(image, axis=axes), # (C,)
+            np.std(image, axis=axes) # (C,)
         )
     elif strategy == "global":
         axes = tuple(np.arange(image.ndim))
         stats = (
-            np.mean(image, axis=axes, keepdims=True), 
-            np.std(image, axis=axes, keepdims=True)
+            np.asarray(np.mean(image, axis=axes))[None], # (1,) 
+            np.asarray(np.std(image, axis=axes))[None] # (1,)
         )
     else:
         raise ValueError(

src/careamics/transforms/normalize.py
@@ -26,8 +26,18 @@ def _reshape_stats(stats: list[float], ndim: int) -> NDArray:
     NDArray
         Reshaped stats.
     """
-    return np.array(stats)[(..., *[np.newaxis] * (ndim - 1))]
-
+    arr = np.array(stats)
+    if arr.ndim == 1:
+        return arr[(..., *[np.newaxis] * (ndim - 1))]
+    elif arr.ndim == ndim:
+        return arr
+    elif arr.ndim <= ndim:
+        return arr[(..., *np.newaxis * (ndim - arr.ndim))]
+    else:
+        raise ValueError(
+            f"Stats array has too many dimensions ({arr.ndim}) compared to the image "
+            f"({ndim})."
+        )
 
 class Normalize(Transform):
     """
@@ -116,7 +126,7 @@ class Normalize(Transform):
         tuple of NDArray
             Transformed patch and target, the target can be returned as `None`.
         """
-        if len(self.image_means) != patch.shape[0]:
+        if self.strategy == "channel-wise" and len(self.image_means) != patch.shape[0]:
             raise ValueError(
                 f"Number of means (got a list of size {len(self.image_means)}) and "
                 f"number of channels (got shape {patch.shape} for C(Z)YX) do not match."