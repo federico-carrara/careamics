f769a54c3010d0e9a1f84e0f1ef7419994002d5f
federico-carrara <federico1.carrara@mail.polimi.it>
2024-10-28 13:43:30 +0100

    feat: code to plot intensity histogram

--- src/careamics/utils/visualization.py
+++ src/careamics/utils/visualization.py
@@ -6,7 +6,8 @@ from IPython.display import HTML
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from numpy.typing import NDArray
-
+import torch
+import numpy as np
 
 def _get_animation(img: NDArray, axis: int) -> FuncAnimation:
     """Get the matplotlib animation.
@@ -67,3 +68,43 @@ def view3D(img: NDArray, axis: int, jupyter: bool) -> Optional[HTML]:
         return HTML(anim.to_jshtml())
     else:    
         plt.show()
+        
+
+def intensity_histograms(
+    imgs: torch.Tensor,
+    max_y: float = None,
+    max_x: float = None
+) -> None:
+    """Plot intensity histograms of a multichannel image.
+    
+    Parameters
+    ----------
+    imgs : torch.Tensor
+        Multichannel image. Shape is (C, [Z], Y, X).
+    max_y : float
+        Maximum y-axis value.
+    max_x : float
+        Maximum x-axis value.
+        
+    Returns
+    -------
+    None
+    """
+    n = imgs.shape[0]
+    fig, ax = plt.subplots(1, n, figsize=(5*n, 5))
+    fig.suptitle(f"Intensity distributions")
+    for i in range(n):
+        img = imgs[i]
+        img = img.flatten()
+        qtiles = np.quantile(img, (0.5, 0.75, 0.95, 0.99))
+        mean = img.mean()
+        std = img.std()
+        ax[i].set_title(f"Intensity distr: FP#{i+1}")
+        ax[i].hist(img, bins=20, color="gray")
+        for qt in qtiles:
+            ax[i].axvline(qt, color='r', linestyle='--', linewidth=1)
+        ax[i].set_ylim(0, max_y)
+        ax[i].set_xlim(0, max_x)
+        print(f"FP#{i+1}: mean: {mean:.2f}, std: {std:.2f}, 50%: {qtiles[0]:.2f}, 75%: {qtiles[1]:.2f}, 95%: {qtiles[2]:.2f}, 99%: {qtiles[3]:.2f}")
+
+    plt.show()
