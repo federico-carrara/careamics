619821800c3d781a7136ad29934c00c5ee1a9fe4
federico-carrara <federico1.carrara@mail.polimi.it>
2024-10-28 13:48:50 +0100

    fix: bugs fixed

--- src/careamics/utils/eval_utils.py
+++ src/careamics/utils/eval_utils.py
@@ -3,57 +3,54 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from careamics.losses.lvae.losses import get_reconstruction_loss, _reconstruction_loss_musplit_denoisplit
 from careamics.lightning import VAEModule
+from careamics.config.tile_information import TileInformation
     
 
-def get_dset_predictions(
+def get_tiled_predictions(
     model: VAEModule,
     dloader: DataLoader,
     mmse_count: int = 1
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, list[float]]:
-    """Get patch-wise predictions from a model for the entire dataset.
+) -> tuple[list[np.ndarray], list[np.ndarray], list[TileInformation]]:
+    """Get tiled predictions from a model for the entire dataset.
 
     Parameters
     ----------
     model : VAEModule
         Lightning model used for prediction.
     dloader : DataLoader
-        DataLoader to predict on.
+        DataLoader to predict on. Dataset must be a `InMemoryTiledPredDataset` object.
     mmse_count : int, optional
         Number of samples to generate for each input and then to average over for
         MMSE estimation, by default 1.
 
     Returns
     -------
-    tuple[np.ndarray, np.ndarray]
+    tuple[list[np.ndarray], list[np.ndarray], list[TileInformation]]
         Tuple containing:
             - predictions: Predicted images for the dataset.
             - predictions_std: Standard deviation of the predicted images.
+            - tiles_info: Information about the tiles coordinates.
     """
     predictions = []
     predictions_std = []
+    tiles_info = []
     with torch.no_grad():
         for batch in tqdm(dloader, desc="Predicting patches"):
-            inp, tar = batch
+            inp, tinfo = batch
             inp = inp.cuda()
-            if tar:
-                tar = tar.cuda()
+            tiles_info.extend(tinfo)
 
             rec_img_list = []
-            logvar_img_list = []
             for _ in range(mmse_count):
-                # get model output
-                rec, out, _ = model(inp)
-
-                # get reconstructed img
+                _, out, _ = model(inp)
+                
                 if model.model.predict_logvar is None:
                     rec_img = out
                     logvar = torch.tensor([-1])
                 else:
                     rec_img, logvar = torch.chunk(out, chunks=2, dim=1)
-                rec_img_list.append(rec_img.cpu().unsqueeze(0)) 
-                logvar_img_list.append(logvar.cpu().unsqueeze(0))
+                rec_img_list.append(rec_img.cpu().unsqueeze(0)) # add MMSE dim
 
             # aggregate results
             samples = torch.cat(rec_img_list, dim=0)
@@ -62,7 +59,4 @@ def get_dset_predictions(
             predictions.append(mmse_imgs.cpu().numpy())
             predictions_std.append(mmse_std.cpu().numpy())
 
-    return (
-        np.concatenate(predictions, axis=0),
-        np.concatenate(predictions_std, axis=0),
-    )
+    return np.concatenate(predictions), np.concatenate(predictions_std), tiles_info
