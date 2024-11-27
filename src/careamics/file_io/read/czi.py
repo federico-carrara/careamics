b095b9839ba180432290e706686fbd4eb498d029
federico-carrara <federico1.carrara@mail.polimi.it>
2024-11-27 15:19:03 +0100

    refac: modified function to read CZI to make it compatible with CAREamics dsets

--- src/careamics/file_io/read/czi.py
+++ src/careamics/file_io/read/czi.py
@@ -6,7 +6,7 @@ from numpy.typing import NDArray
 
 
 def read_czi(
-    path: Union[str, Path], load_metadata: bool =  False
+    path: Union[str, Path], *args: list, **kwargs: dict
 ) -> Union[NDArray, tuple[NDArray, dict]]:
     """Load a CZI file and return the image and optionally the metadata.
     
@@ -14,8 +14,6 @@ def read_czi(
     ----------
     path : Union[str, Path]
         The path to the CZI file.
-    load_metadata : bool, optional
-        Whether to load the metadata, by default False.
     
     Returns
     -------
@@ -24,8 +22,4 @@ def read_czi(
     """
     with czi.CziFile(path) as f:
         img = f.asarray()
-        if load_metadata:
-            metadata = f.metadata()
-            return img, metadata
-        else:
-            return img
+        return img.squeeze()
