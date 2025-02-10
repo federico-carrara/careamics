faf89b52fc0813c60677e33fda008ef6b78b538d
Federico Carrara <74301866+federico-carrara@users.noreply.github.com>
2025-02-10 18:14:28 +0100

    feat: added LPIPS metric + refactored Multiscale SSIM function (#14)
    * feat: added function `lpips()` to compute LPIPS metric
    
    * refac: refactored `multiscale_ssim()` + added assertion to `lpips()` to check normalization of input.
    
    * refac + fix: added GPU support for LPIPS + solved many bigs + improved docstrings

--- src/careamics/utils/metrics.py
+++ src/careamics/utils/metrics.py
@@ -10,6 +10,7 @@ import numpy as np
 import torch
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 from torchmetrics.image import MultiScaleStructuralSimilarityIndexMeasure
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 
 # TODO: does this add additional dependency?
 
@@ -198,7 +199,7 @@ class RunningPSNR:
         return 20 * torch.log10((self.max - self.min) / rmse)
 
 
-def _range_invariant_multiscale_ssim(
+def range_invariant_multiscale_ssim(
     gt_: Union[np.ndarray, torch.Tensor], pred_: Union[np.ndarray, torch.Tensor]
 ) -> float:
     """Compute range invariant multiscale SSIM for a single channel.
@@ -212,9 +213,9 @@ def _range_invariant_multiscale_ssim(
     Parameters
     ----------
     gt_ : Union[np.ndarray, torch.Tensor]
-        Ground truth image with shape (N, H, W).
+        Ground truth image with shape (N, [Z], Y, X).
     pred_ : Union[np.ndarray, torch.Tensor]
-        Predicted image with shape (N, H, W).
+        Predicted image with shape (N, [Z], Y, X).
 
     Returns
     -------
@@ -222,18 +223,29 @@ def _range_invariant_multiscale_ssim(
         Range invariant multiscale SSIM value.
     """
     shape = gt_.shape
-    gt_ = torch.Tensor(gt_.reshape((shape[0], -1)))
-    pred_ = torch.Tensor(pred_.reshape((shape[0], -1)))
+    gt_ = torch.tensor(gt_.reshape((shape[0], -1)), dtype=torch.float32)
+    pred_ = torch.tensor(pred_.reshape((shape[0], -1)), dtype=torch.float32)
     gt_ = _zero_mean(gt_)
     pred_ = _zero_mean(pred_)
     pred_ = _fix(gt_, pred_)
     pred_ = pred_.reshape(shape)
     gt_ = gt_.reshape(shape)
-
+    # add channel dimension
+    pred_ = pred_[:, None]
+    gt_ = gt_[:, None]
+    
+    
     ms_ssim = MultiScaleStructuralSimilarityIndexMeasure(
         data_range=gt_.max() - gt_.min()
     )
-    return ms_ssim(torch.Tensor(pred_[:, None]), torch.Tensor(gt_[:, None])).item()
+    if len(pred_.shape) == 5: # 3D input
+        # iterate over Z-stack
+        return np.mean([
+            ms_ssim(pred_[:, :, i], gt_[:, :, i]).item()
+            for i in range(pred_.shape[2])
+        ])
+    else: # 2D input
+        return ms_ssim(pred_, gt_).item()
 
 
 def multiscale_ssim(
@@ -245,15 +257,12 @@ def multiscale_ssim(
 
     It allows to use either standard multiscale SSIM or its range-invariant version.
 
-    NOTE: images fed to this function should have channels dimension as the last one.
-    # TODO: do we want to allow this behavior? or we want the usual (N, C, H, W)?
-
     Parameters
     ----------
     gt_ : Union[np.ndarray, torch.Tensor]
-        Ground truth image with shape (N, H, W, C).
+        Ground truth image with shape (N, C, [Z], Y, X).
     pred_ : Union[np.ndarray, torch.Tensor]
-        Predicted image with shape (N, H, W, C).
+        Predicted image with shape (N, C, [Z], Y, X).
     range_invariant : bool
         Whether to use standard or range invariant multiscale SSIM.
 
@@ -264,10 +273,10 @@ def multiscale_ssim(
     """
     ms_ssim_values = {}
     for ch_idx in range(gt_.shape[-1]):
-        tar_tmp = gt_[..., ch_idx]
-        pred_tmp = pred_[..., ch_idx]
+        tar_tmp = gt_[:, ch_idx, ...]
+        pred_tmp = pred_[:, ch_idx, ...]
         if range_invariant:
-            ms_ssim_values[ch_idx] = _range_invariant_multiscale_ssim(
+            ms_ssim_values[ch_idx] = range_invariant_multiscale_ssim(
                 gt_=tar_tmp, pred_=pred_tmp
             )
         else:
@@ -278,7 +287,7 @@ def multiscale_ssim(
                 torch.Tensor(pred_tmp[:, None]), torch.Tensor(tar_tmp[:, None])
             ).item()
 
-    return [ms_ssim_values[i] for i in range(gt_.shape[-1])]  # type: ignore
+    return [ms_ssim_values[i] for i in range(gt_.shape[1])]  # type: ignore
 
 
 def _avg_psnr(target: np.ndarray, prediction: np.ndarray, psnr_fn: Callable) -> float:
@@ -366,3 +375,60 @@ def avg_ssim(
         for i in range(len(target))
     ]
     return np.mean(ssim), np.std(ssim)
+
+
+def lpips(
+    prediction: Union[np.ndarray, torch.Tensor], 
+    target: Union[np.ndarray, torch.Tensor]
+) -> float:
+    """Compute the Learned Perceptual Image Patch Similarity (LPIPS) over images.
+    
+    If inputs are 3D, LPIPS is averaged over the Z-stack.
+    
+    NOTES:
+    - LPIPS can use different networks. Here we use the SqueezeNet model.
+    - The inputs are expected to be normalized in the range [0, 1].
+    - We use the mean reduction, i.e., the LPIPS value is averaged over the batch.
+
+    Parameters
+    ----------
+    prediction : Union[np.ndarray, torch.Tensor]
+        Array of predicted images, shape is (N, C, [Z], Y, X).
+    target : Union[np.ndarray, torch.Tensor]
+        Array of ground truth images, shape is (N, C, [Z], Y, X).
+
+    Returns
+    -------
+    float
+        LPIPS value over the batch.
+    """
+    assert prediction.shape == target.shape, "Prediction and target shapes must match."
+    assert prediction.max() <= 1 and prediction.min() >= 0, (
+        "Prediction must be normalized in [0, 1]."
+    )
+    assert target.max() <= 1 and target.min() >= 0, (
+        "Target must be normalized in [0, 1]."
+    )
+    
+    # check if GPU is available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # compute LPIPS
+    lpips = LearnedPerceptualImagePatchSimilarity(
+        net_type='squeeze', reduction='mean', normalize=True
+    ).to(device)
+    
+    if len(prediction.shape) == 5: # 3D input
+        # iterate over Z-stack
+        return np.mean([
+            lpips(
+                torch.tensor(prediction[:, :, i], device=device, dtype=torch.float32),
+                torch.tensor(target[:, :, i], device=device, dtype=torch.float32)
+            ).item()
+            for i in range(prediction.shape[2])
+        ])
+    else:
+        return lpips(
+            torch.tensor(prediction, device=device, dtype=torch.float32),
+            torch.tensor(target, device=device, dtype=torch.float32)
+        ).item()
